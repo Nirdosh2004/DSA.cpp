15e9207ca97b8a9bf372625a262e6c47c6e46613
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-03-26 19:47:56 +0530

    Create One.cpp

---
+++ Linked list/One.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+    
+    node( int data1){
+        data = data1;
+        next = NULL;
+    }
+};
+
+int main(){
+    vector<int> arr = {2,5,3,7};
+    node* y = new node(arr[2]);
+    node* x = new node(arr[0]);
+    cout<<x<<endl;
+    cout<<y;
+    
+    
+    return 0;
+}
