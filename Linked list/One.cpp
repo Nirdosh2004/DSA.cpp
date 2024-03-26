73e39d030d4b1859b1a731919532af205cf8caa5
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-03-26 19:50:59 +0530

    Update One.cpp

--- Linked list/One.cpp
+++ Linked list/One.cpp
@@ -7,16 +7,16 @@ class node{
     int data;
     node* next;
     public:
-    node( int data1){
+    node( int data1 , node* next1){
         data = data1;
-        next = NULL;
+        next = next1;
     }
 };
 
 int main(){
     vector<int> arr = {2,5,3,7};
-    node* y = new node(arr[2]);
-    node* x = new node(arr[0]);
+    node* y = new node(arr[2] , nullptr);
+    node* x = new node(arr[0] , nullptr);
     cout<<x<<endl;
     cout<<y;
     
